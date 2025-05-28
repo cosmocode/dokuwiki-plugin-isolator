64656a41e398df30153e1fa133f73bc26aa047f0
Andreas Gohr <gohr@cosmocode.de>
2025-05-28 14:16:34 +0200

    feat: add IdUtils class with namespace and relative ID utilities

---
+++ IdUtils.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace dokuwiki\plugin\isolator;
+
+class IdUtils
+{
+    public static function isInNamespace($id, $namespace)
+    {
+        if ($namespace === '') {
+            return true; // root namespace
+        }
+
+        return str_starts_with($id, "$namespace:");
+    }
+
+    public static function isInSameNamespace($id, $namespace)
+    {
+        $idns = getNS($id);
+        return $idns === $namespace;
+    }
+
+    public static function getRelativeID($id, $reference)
+    {
+        $idNS = explode(':', $id);
+        $refNS = explode(':', $reference);
+
+        // Remove common namespace parts
+        while (!empty($idNS) && !empty($refNS) && $idNS[0] === $refNS[0]) {
+            array_shift($idNS);
+            array_shift($refNS);
+        }
+
+        // Add `..` for each remaining part in the reference namespace
+        $relative = str_repeat('..:', count($refNS));
+
+        // Append the remaining parts of the ID
+        $relative .= implode(':', $idNS);
+
+        return $relative;
+    }
+}
