c8428f916ca3ed72f553ba14ef9a6cf34f8bbb04
Andreas Gohr <gohr@cosmocode.de>
2025-05-28 14:35:52 +0200

    refactor: improve getRelativeID method with namespace handling and relative path generation

--- IdUtils.php
+++ IdUtils.php
@@ -25,14 +25,16 @@ class IdUtils
             throw new RuntimeException('Empty ID is not allowed');
         }
 
-        $idns = (string) getNS($id);
+        $idns = (string)getNS($id);
         return $idns === $namespace;
     }
 
-    public static function getRelativeID($id, $reference)
+    public static function getRelativeID($targetId, $reference)
     {
-        $idNS = explode(':', $id);
+        $idNS = explode(':', $targetId);
+        $id = array_pop($idNS);
         $refNS = explode(':', $reference);
+        array_pop($refNS);
 
         // Remove common namespace parts
         while (!empty($idNS) && !empty($refNS) && $idNS[0] === $refNS[0]) {
@@ -45,6 +47,15 @@ class IdUtils
 
         // Append the remaining parts of the ID
         $relative .= implode(':', $idNS);
+
+        // Remove trailing colon if any
+        $relative = rtrim($relative, ':');
+
+        // Add the ID itself
+        $relative .= ($relative === '' ? '' : ':') . $id;
+
+        // Ensure we are always relative to the current namespace. This is specific to the isolator plugin
+        if ($relative[0] !== '.') $relative = '.' . $relative;
 
         return $relative;
     }
