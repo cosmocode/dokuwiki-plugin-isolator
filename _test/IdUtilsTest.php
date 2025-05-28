4efedf4c7b0b0719ab6fe9e368f414c815066ca0
Andreas Gohr <gohr@cosmocode.de>
2025-05-28 14:13:38 +0200

    test: add placeholder test class for IdUtils

---
+++ _test/IdUtilsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace dokuwiki\plugin\isolator\test;
+
+use DokuWikiTest;
+
+/**
+ * FIXME tests for the isolator plugin
+ *
+ * @group plugin_isolator
+ * @group plugins
+ */
+class IdUtilsTest extends DokuWikiTest
+{
+    /**
+     * @todo implement tests
+     */
+    public function testSomething()
+    {
+
+    }
+}
