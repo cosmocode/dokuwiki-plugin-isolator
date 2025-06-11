9e9c07dac5ac7577e0609739bd09f913a39b5365
Andreas Gohr <gohr@cosmocode.de>
2025-06-11 09:21:05 +0200

    use fake PDF files for testing to avoid content checks
    Our upload mechanism checks content types for images. Since our
    documents are fake, we can't have that.

--- _test/ProcessorTest.php
+++ _test/ProcessorTest.php
@@ -18,17 +18,17 @@ class ProcessorTest extends DokuWikiTest
         parent::setUp();
 
         // Create test pages with media references
-        saveWikiText('test:page1', '{{other:image4.jpg}} Some content with external media', 'Test setup');
-        saveWikiText('test:page2', '{{test:image1.jpg}} Content with local media', 'Test setup');
-        saveWikiText('test:subns:page3', '{{test:image2.png}} Content in subnamespace', 'Test setup');
-        saveWikiText('other:page4', '{{test:image1.jpg}} Content in other namespace', 'Test setup');
+        saveWikiText('test:page1', '{{other:document4.pdf}} Some content with external media', 'Test setup');
+        saveWikiText('test:page2', '{{test:document1.pdf}} Content with local media', 'Test setup');
+        saveWikiText('test:subns:page3', '{{test:document2.pdf}} Content in subnamespace', 'Test setup');
+        saveWikiText('other:page4', '{{test:document1.pdf}} Content in other namespace', 'Test setup');
 
         // Create test media files
         $testMediaFiles = [
-            'test:image1.jpg' => 'fake jpg content',
-            'test:image2.png' => 'fake png content',
-            'test:subns:image3.gif' => 'fake gif content',
-            'other:image4.jpg' => 'fake jpg content'
+            'test:document1.pdf' => 'fake media content',
+            'test:document2.pdf' => 'fake media content',
+            'test:subns:document3.pdf' => 'fake media content',
+            'other:document4.pdf' => 'fake media content'
         ];
 
         foreach ($testMediaFiles as $mediaId => $content) {
@@ -45,17 +45,17 @@ class ProcessorTest extends DokuWikiTest
 
         $this->assertTrue($result['changed']);
         $this->assertNotEmpty($result['copyList']);
-        $this->assertArrayHasKey('other:image4.jpg', $result['copyList']);
-        $this->assertEquals('test:image4.jpg', $result['copyList']['other:image4.jpg']);
+        $this->assertArrayHasKey('other:document4.pdf', $result['copyList']);
+        $this->assertEquals('test:document4.pdf', $result['copyList']['other:document4.pdf']);
 
         // Verify the content was actually changed to use relative reference
-        $this->assertStringContainsString('{{image4.jpg}}', $result['new']);
-        $this->assertStringNotContainsString('{{other:image4.jpg}}', $result['new']);
+        $this->assertStringContainsString('{{document4.pdf}}', $result['new']);
+        $this->assertStringNotContainsString('{{other:document4.pdf}}', $result['new']);
 
         // Verify the media file was actually copied
-        $copiedFile = mediaFN('test:image4.jpg');
+        $copiedFile = mediaFN('test:document4.pdf');
         $this->assertFileExists($copiedFile);
-        $this->assertEquals('fake jpg content', file_get_contents($copiedFile));
+        $this->assertEquals('fake media content', file_get_contents($copiedFile));
     }
 
     public function testProcessPageWithLocalMedia()
@@ -68,11 +68,11 @@ class ProcessorTest extends DokuWikiTest
 
         // But should still adjust reference to be relative if it was absolute
         $this->assertTrue($result['changed']);
-        $this->assertStringContainsString('{{image1.jpg}}', $result['new']);
-        $this->assertStringNotContainsString('{{test:image1.jpg}}', $result['new']);
+        $this->assertStringContainsString('{{document1.pdf}}', $result['new']);
+        $this->assertStringNotContainsString('{{test:document1.pdf}}', $result['new']);
 
         // Verify original media file still exists and wasn't moved
-        $originalFile = mediaFN('test:image1.jpg');
+        $originalFile = mediaFN('test:document1.pdf');
         $this->assertFileExists($originalFile);
     }
 
@@ -86,19 +86,19 @@ class ProcessorTest extends DokuWikiTest
         $processorStrict = new Processor('test', false, true);
         $resultStrict = $processorStrict->processPage('test:subns:page3');
 
-        // In non-strict mode, test:image2.png should be considered "in namespace"
+        // In non-strict mode, test:document2.pdf should be considered "in namespace"
         // but reference should still be adjusted to relative
         $this->assertTrue($resultNonStrict['changed']);
         $this->assertEmpty($resultNonStrict['copyList']); // No copying in non-strict mode
 
-        // In strict mode, test:image2.png should be moved to test:subns:
+        // In strict mode, test:document2.pdf should be moved to test:subns:
         $this->assertTrue($resultStrict['changed']);
         $this->assertNotEmpty($resultStrict['copyList']); // Should copy in strict mode
 
         // Verify the media file was actually copied in strict mode
-        $copiedFile = mediaFN('test:subns:image2.png');
+        $copiedFile = mediaFN('test:subns:document2.pdf');
         $this->assertFileExists($copiedFile);
-        $this->assertEquals('fake png content', file_get_contents($copiedFile));
+        $this->assertEquals('fake media content', file_get_contents($copiedFile));
     }
 
     public function testIsolateProcessesMultiplePages()
@@ -118,19 +118,19 @@ class ProcessorTest extends DokuWikiTest
         $this->assertNotEmpty($testPages);
 
         // Verify that media files were actually copied where needed
-        $copiedFile = mediaFN('test:image4.jpg');
+        $copiedFile = mediaFN('test:document4.pdf');
         $this->assertFileExists($copiedFile);
     }
 
     public function testUseCaseExample()
     {
         // Create the specific scenario from UseCase.md
-        saveWikiText('foo:bar:baz', '{{foo:qux.jpg}} Content with media reference', 'Test setup');
+        saveWikiText('foo:bar:baz', '{{foo:qux.pdf}} Content with media reference', 'Test setup');
 
         // Create the media file
-        $mediaFile = mediaFN('foo:qux.jpg');
+        $mediaFile = mediaFN('foo:qux.pdf');
         io_makeFileDir($mediaFile);
-        file_put_contents($mediaFile, 'fake jpg content');
+        file_put_contents($mediaFile, 'fake media content');
 
         // Test normal mode - should keep media in foo: but adjust reference
         $processor = new Processor('foo', false, false);
@@ -138,8 +138,8 @@ class ProcessorTest extends DokuWikiTest
 
         $this->assertTrue($result['changed']);
         $this->assertEmpty($result['copyList']); // No copying needed in normal mode
-        $this->assertStringContainsString('{{..:qux.jpg}}', $result['new']);
-        $this->assertStringNotContainsString('{{foo:qux.jpg}}', $result['new']);
+        $this->assertStringContainsString('{{..:qux.pdf}}', $result['new']);
+        $this->assertStringNotContainsString('{{foo:qux.pdf}}', $result['new']);
 
         // Test strict mode - should copy media to foo:bar: and adjust reference
         $processorStrict = new Processor('foo', false, true);
@@ -147,32 +147,32 @@ class ProcessorTest extends DokuWikiTest
 
         $this->assertTrue($resultStrict['changed']);
         $this->assertNotEmpty($resultStrict['copyList']);
-        $this->assertArrayHasKey('foo:qux.jpg', $resultStrict['copyList']);
-        $this->assertEquals('foo:bar:qux.jpg', $resultStrict['copyList']['foo:qux.jpg']);
-        $this->assertStringContainsString('{{qux.jpg}}', $resultStrict['new']);
+        $this->assertArrayHasKey('foo:qux.pdf', $resultStrict['copyList']);
+        $this->assertEquals('foo:bar:qux.pdf', $resultStrict['copyList']['foo:qux.pdf']);
+        $this->assertStringContainsString('{{qux.pdf}}', $resultStrict['new']);
 
         // Verify the media file was actually copied in strict mode
-        $copiedFile = mediaFN('foo:bar:qux.jpg');
+        $copiedFile = mediaFN('foo:bar:qux.pdf');
         $this->assertFileExists($copiedFile);
-        $this->assertEquals('fake jpg content', file_get_contents($copiedFile));
+        $this->assertEquals('fake media content', file_get_contents($copiedFile));
 
     }
 
     public function testAbsolutePathConversion()
     {
         // Create a page with absolute media reference
-        saveWikiText('test:subpage', '{{test:image1.jpg}} Content with absolute reference', 'Test setup');
+        saveWikiText('test:subpage', '{{test:document1.pdf}} Content with absolute reference', 'Test setup');
 
         $processor = new Processor('test', false, false);
         $result = $processor->processPage('test:subpage');
 
         $this->assertTrue($result['changed']);
         $this->assertEmpty($result['copyList']); // No copying needed, just reference adjustment
-        $this->assertStringContainsString('{{image1.jpg}}', $result['new']);
-        $this->assertStringNotContainsString('{{test:image1.jpg}}', $result['new']);
+        $this->assertStringContainsString('{{document1.pdf}}', $result['new']);
+        $this->assertStringNotContainsString('{{test:document1.pdf}}', $result['new']);
 
         // Verify original media file still exists
-        $originalFile = mediaFN('test:image1.jpg');
+        $originalFile = mediaFN('test:document1.pdf');
         $this->assertFileExists($originalFile);
     }
 
